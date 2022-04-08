242c381e0a6eafbbe5f999142918bff43f7114a8
Edward M Coleman <emcoleman@email.wm.edu>
2022-04-08 17:18:44 -0400

    Add Database information and first entry from app to database.

--- app/src/main/java/com/example/parkingpal/MainActivity.java
+++ app/src/main/java/com/example/parkingpal/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Commit Edward
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
