ee4ab5129d5ea1d7847d662d5d0f74a8647bd1b9
jpedone <jpedone@wm.edu>
2022-04-07 22:10:45 -0400

    linked acc info page to permit type pg with button

--- app/src/main/java/com/example/parkingpal/CreateAccount.java
+++ app/src/main/java/com/example/parkingpal/CreateAccount.java
@@ -2,7 +2,10 @@ package com.example.parkingpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button buttonOne = findViewById(R.id.account_info_next_button);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+                //when next button is clicked, user is brought to the screen to choose their permit type
+                Intent activity2Intent = new Intent(getApplicationContext(), ChoosePermit.class);
+                startActivity(activity2Intent);
+            }
+        });
     }
 }
