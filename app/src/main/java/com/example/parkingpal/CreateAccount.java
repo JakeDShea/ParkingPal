c0c15c724e07057875c886e6862e442cd989398d
jpedone <jpedone@wm.edu>
2022-04-10 17:10:10 -0400

    updated some links/ input fields

--- app/src/main/java/com/example/parkingpal/CreateAccount.java
+++ app/src/main/java/com/example/parkingpal/CreateAccount.java
@@ -13,6 +13,8 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button b1 = (Button) findViewById(R.id.button); //back to login button
+
 
         Button buttonOne = findViewById(R.id.account_info_next_button);
         buttonOne.setOnClickListener(new View.OnClickListener() {
@@ -20,6 +22,16 @@ public class CreateAccount extends AppCompatActivity {
                 System.out.println("Button Clicked");
                 //when next button is clicked, user is brought to the screen to choose their permit type
                 Intent activity2Intent = new Intent(getApplicationContext(), ChoosePermit.class);
+                startActivity(activity2Intent);
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+                //"or login" button clicked, bring back to landing page
+                Intent activity2Intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(activity2Intent);
             }
         });
--- app/src/main/java/com/example/parkingpal/MainActivity.java
+++ app/src/main/java/com/example/parkingpal/MainActivity.java
@@ -2,14 +2,67 @@ package com.example.parkingpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
+    EditText ed1,ed2;
+
+    TextView tx1;
+    int counter = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Commit Edward
+
+        b1 = (Button)findViewById(R.id.button); //login button
+        ed1 = (EditText)findViewById(R.id.editText); //email
+        ed2 = (EditText)findViewById(R.id.editText2); //password
+
+        //if log in button clicked, take them to main page....
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1.getText().toString().equals("admin") &&
+                        ed2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...",Toast.LENGTH_SHORT).show();
+                    //will need to link the login putton to the home page once implemented
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+                    //counting to limit amount of invalid login attempts
+                    tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        b1.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        //clicked the sign up button - take to sign up page
+        b2 = (Button)findViewById(R.id.button2); //sign up button
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+                //when next button is clicked, user is brought to the screen to choose their permit type
+                Intent activity2Intent = new Intent(getApplicationContext(), CreateAccount.class);
+                startActivity(activity2Intent);
+            }
+        });
     }
 }
