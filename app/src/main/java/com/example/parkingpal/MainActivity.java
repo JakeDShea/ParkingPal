6d7b49d51482dadebf373ad6764a4a883e5808af
Jake Denis Shea <jdshea@email.wm.edu>
2022-04-10 23:02:14 -0400

    Add steps to work with parking limits and spots available

--- app/src/main/java/com/example/parkingpal/MainActivity.java
+++ app/src/main/java/com/example/parkingpal/MainActivity.java
@@ -3,17 +3,22 @@ package com.example.parkingpal;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button2 = (Button) findViewById(R.id.testParkingButton);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openParkActivity();
+            }
+        });
+
     }
     public void openMapActivity(){
         setContentView(R.layout.activity_mapscreen);
@@ -44,5 +57,16 @@ public class MainActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.frame_layout,fragment)
                 .commit();
+    }
+
+    public void openParkActivity(){
+        // Creates a random seed
+        Random generator = new Random();
+
+        // Sets up the intent of this method with values to pass along
+        Intent intent = new Intent(this, ParkingActivity.class);
+
+        // Starts the activity
+        startActivity(intent);
     }
 }
